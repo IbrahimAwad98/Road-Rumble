b3a4c65e967f5bfacaac9dd83e0ef9937d5f5788
Ibrahim Awad <ibrahimawad98@hotmail.com>
2025-04-12 14:52:33 +0200

    Add test and debug mode with logging to testlog.txt

--- source/main.c
+++ source/main.c
@@ -3,18 +3,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
+
+// filer
+#include "cleanup.h"
 #include "game.h"
 #include "sdl_init.h"
 #include "resources.h"
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL.h>
-#include "cleanup.h"
-#include <math.h>
+#include "test_debug.h"
 
 int main(int argc, char **argv)
 {
+
+    // skriv ut test och debugg utdata för en fil
+    freopen("testlog.txt", "w", stdout);
+    freopen("testlog.txt", "a", stderr);
+
     // Strukturen som håller alla resurser
     GameResources res = {0};
+    // två variabel för test-debug hantering
+    bool testMode = false, debugMode = false;
+
+    // flaggor
+    for (int i = 1; i < argc; i++)
+    {
+        // hantera båda stor och små bokstav
+        if (strcasecmp(argv[i], "--test") == 0)
+        {
+            testMode = true;
+        }
+        if (strcasecmp(argv[i], "--debug") == 0)
+        {
+            debugMode = true;
+        }
+    }
 
     // Initiera SDL, fönster, ljud, nätverk
     if (!initSDL(&res))
@@ -31,7 +55,23 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    printf("Resources loaded successfully!\n");
+    if (testMode)
+    {
+        bool testPassed = true;
+        runAllTests(&res, &testPassed, debugMode);
+        if (testPassed)
+        {
+            printf("TEST RESULTAT: PASSED.\n");
+            printf("Everything went fine! Test completed without errors.\n");
+            return 0;
+        }
+        else
+        {
+            printf("TEST RESULTAT: FAILED.\n");
+            printf("One or more tests failed. Check the log above for details.\n");
+            return 1;
+        }
+    }
 
     gameLoop(&res); // Starta huvudloopen för spelet
     cleanup(&res);  // Rensa och avsluta
